0024447438f62909cc029fd1bb3b09334154b25e
IT24510004 <it24510004@my.sliit.lk>
2025-09-14 22:43:46 +0530

    IT24510004Lab06

---
+++ Lab Sheet 06/Lab06/IT 24510004_ PS labsheet 6.R
@@ -0,0 +1,14 @@
+# 1) i) X ~ Binomial(n=50,p=0.85)
+
+# ii) 
+1 - pbinom(46,50,0.85,lower.tail=TRUE)
+
+
+# 2) i) X = Number of customer calls received in an hour
+
+# ii) X ~ Poisson(λ = 12)
+
+# iii)
+dpois(15, lambda = 12)
+
+
